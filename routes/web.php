11c797efc5bd8b6137b762c962f0da5e10960778
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-10-18 16:47:12 +0200

    add seeder

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ComicController;
 
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
